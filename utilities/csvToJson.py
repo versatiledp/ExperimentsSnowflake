759323bdf90f882761192b44778937f9903dfa29
versatiledp <75915389+versatiledp@users.noreply.github.com>
2020-12-13 12:24:45 -0600

    Update csvToJson.py

--- utilities/csvToJson.py
+++ utilities/csvToJson.py
@@ -6,7 +6,7 @@
 import csv 
 import json 
   
-csvfile2019 = open("C:\\snowflake\\rawdata\\effy2019.csv", "r")
+csvfile2019 = open("C:\\snowflake\\inputData\\rawdata\\effy2019.csv", "r")
 jsonfile2019 = open( "C:\\snowflake\\inputData\\effy2019_rv.json" , 'w')
 #dict_reader = csv.DictReader(csvfile2019)
 reader2019 = csv.DictReader(csvfile2019)
@@ -16,7 +16,7 @@ for row in reader2019:
 csvfile2019.close()
 jsonfile2019.close()
 
-csvfile2018 = open("C:\\snowflake\\rawdata\\effy2018_rv.csv", "r")
+csvfile2018 = open("C:\\snowflake\\inputData\\rawdata\\effy2018_rv.csv", "r")
 jsonfile2018 = open( "C:\\snowflake\\inputData\\effy2018_rv.json" , 'w')
 #dict_reader = csv.DictReader(csvfile2018)
 reader2018 = csv.DictReader(csvfile2018)
@@ -26,7 +26,7 @@ for row in reader2018:
 csvfile2018.close()
 jsonfile2018.close()
 
-csvfile2017 = open("C:\\snowflake\\rawdata\\effy2017_rv.csv", "r")
+csvfile2017 = open("C:\\snowflake\\inputData\\rawdata\\effy2017_rv.csv", "r")
 jsonfile2017 = open( "C:\\snowflake\\inputData\\effy2017_rv.json" , 'w')
 #dict_reader = csv.DictReader(csvfile2017)
 reader2017 = csv.DictReader(csvfile2017)
